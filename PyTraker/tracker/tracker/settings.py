35e659036038be771068dfa13a63dfc876b9718a
ese-omene <55164935+ese-omene@users.noreply.github.com>
2020-06-30 18:30:34 -0400

    added back images

--- PyTraker/tracker/tracker/settings.py
+++ PyTraker/tracker/tracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'k*%okts+%4q&7ml+^tq&hq=iz-2ezp
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['py-traker.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
